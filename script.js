1f5d14d52b03b1df43be54b7216b116b06f13343
Demetrusa <demetre.kurdadze20b@geolab.edu.gegit config --global user.name Demetrusacd /Desktop/Users/Git/Demegit config --global user.email demetre.kurdadze20b@geolab.edu.ge>
2022-01-27 23:34:57 +0400

    h3 style changed

--- script.js
+++ script.js
@@ -22,3 +22,22 @@ button.addEventListener('click', function(){
       h3.style.color = 'black';
     }
 })
+
+h3.addEventListener('click', function(){
+  let backgroundColor = color[Math.floor(Math.random() * color.length)]
+  let body = document.querySelector('body');
+
+  body.style.background = backgroundColor;
+  h2.innerHTML = backgroundColor;
+  if(backgroundColor === 'black'){
+    h2.style.color = 'white';
+    h3.style.color = 'white';
+  }
+  if(backgroundColor === 'black'){
+    h3.style.color = 'white';
+  }
+  else{
+    h2.style.color = 'black';
+    h3.style.color = 'black';
+  }
+})
